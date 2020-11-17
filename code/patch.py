ed219a32c17a41da608d1066d0828ef03f850c94
gamestabled <chandlerm2016@yahoo.com>
2020-11-16 21:19:24 -0500

    patch.py now generates patch_symbols.json

--- code/patch.py
+++ code/patch.py
@@ -2,6 +2,7 @@ import os
 import struct
 import subprocess
 import sys
+import json
 
 elf = sys.argv[1]
 result = subprocess.run([os.environ["DEVKITARM"] + r'/bin/arm-none-eabi-objdump', '--section-headers', elf], stdout=subprocess.PIPE)
@@ -9,6 +10,17 @@ lines = str(result.stdout).split('\\n')
 sectionsInfo = [line.split()[1:6] for line in lines if line.split() and line.split()[0].isdigit()]
 sections = ((sec[0], int(sec[2],16), int(sec[4],16), int(sec[1],16)) for sec in sectionsInfo if int(sec[2],16) != 0)
 
+# Put here the symbols from the patch which are needed by the app
+desiredSymbols = ("rItemOverrides")
+
+nmResult = subprocess.run([os.environ["DEVKITARM"] + r'/bin/arm-none-eabi-nm', elf], stdout=subprocess.PIPE)
+nmLines = str(nmResult.stdout).split('\\n')
+symbolsInfo = [line.split() for line in nmLines if len(line.split()) >= 2 and line.split()[2] in desiredSymbols]
+symbols = {sym[2]:hex(int(sym[0],16)) for sym in symbolsInfo}
+symbolsJson = json.dumps(symbols, indent=4)
+with open("patch_symbols.json", 'w') as syms:
+    syms.write(symbolsJson)
+print("wrote desired symbols to patch_symbols.json")
 
 off = lambda vaddr: struct.pack(">I",vaddr - 0x100000)[1:]
 sz = lambda size: struct.pack(">H", size)
@@ -16,9 +28,6 @@ sz = lambda size: struct.pack(">H", size)
 ips = b'PATCH'
 with open(elf, 'rb') as e:
     for name, vaddr, offset, size in sections:
-
-        # if vaddr >= 0x4CA000 and vaddr < 0x5C7000: #this is not good lol just trying to avoid __GNU_EH_FRAME_HDR section
-        #     continue
 
         e.seek(offset, 0)
         while size > 65535:
