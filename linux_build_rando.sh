4e642fce53ec22445d1cbf458f3dfad1ad4ffa73
PhlexPlexico <flexing@phlexplexi.co>
2021-03-22 14:12:35 -0600

    More requirements.

--- linux_build_rando.sh
+++ linux_build_rando.sh
@@ -3,7 +3,9 @@
 gather_required_files() {
   echo "Our python version: "
   python3 -V
-  sudo apt update && sudo apt install automake build-essential wget unzip zip -y
+  sudo apt update && sudo apt install automake build-essential wget unzip zip  -y
+  sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
+    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
   sudo wget https://www.python.org/ftp/python/3.8.5/Python-3.8.5.tgz
   sudo tar xzf Python-3.8.5.tgz
   cd Python-3.8.5
