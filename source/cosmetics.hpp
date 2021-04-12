7fb53b047205c244017ecdd099e401e5d66955b4
Lioncash <mathew1800@gmail.com>
2021-04-12 06:49:42 -0400

    cosmetics: Make strings non-allocating where applicable

--- source/cosmetics.hpp
+++ source/cosmetics.hpp
@@ -5,14 +5,14 @@
 #include <vector>
 
 namespace Cosmetics {
-  const std::string RANDOM_CHOICE_STR = "Random Choice";
-  const std::string RANDOM_COLOR_STR  = "Random Color";
-  const std::string CUSTOM_COLOR_STR  = "Custom #FFFFFF";
-  const std::string CUSTOM_COLOR_PREFIX = "Custom #";
-                                        /*--------------------------------------------------*/
-  const std::string RANDOM_CHOICE_DESC = "A random color from the list of colors will be\nchosen.";
-  const std::string RANDOM_COLOR_DESC  = "A completely random color will be chosen.";
-  const std::string CUSTOM_COLOR_DESC  = "Press A and type in a custom 6 digit hex color.";
+  constexpr std::string_view RANDOM_CHOICE_STR = "Random Choice";
+  constexpr std::string_view RANDOM_COLOR_STR  = "Random Color";
+  constexpr std::string_view CUSTOM_COLOR_STR  = "Custom #FFFFFF";
+  constexpr std::string_view CUSTOM_COLOR_PREFIX = "Custom #";
+
+  constexpr std::string_view RANDOM_CHOICE_DESC = "A random color from the list of colors will be\nchosen.";
+  constexpr std::string_view RANDOM_COLOR_DESC  = "A completely random color will be chosen.";
+  constexpr std::string_view CUSTOM_COLOR_DESC  = "Press A and type in a custom 6 digit hex color.";
 
   enum CosmeticSettings {
     RANDOM_CHOICE,
@@ -28,13 +28,13 @@ namespace Cosmetics {
     float a;
   };
 
-  extern const std::array<std::string, 13> gauntletColors;
-  extern std::vector<std::string> gauntletOptions;
-  extern std::vector<std::string_view> cosmeticDescriptions;
+  extern const std::array<std::string_view, 13> gauntletColors;
+  extern const std::vector<std::string> gauntletOptions;
+  extern const std::vector<std::string_view> cosmeticDescriptions;
 
-  bool ValidHexString(std::string hexStr);
-  Color_RGB HexStrToColorRGB(std::string hexStr);
-  std::string CustomColorOptionText(std::string color);
-  std::string GetCustomColor(std::string str);
+  bool ValidHexString(std::string_view hexStr);
+  Color_RGB HexStrToColorRGB(const std::string& hexStr);
+  std::string CustomColorOptionText(std::string_view color);
+  std::string GetCustomColor(std::string_view str);
   std::string RandomColor();
 } //namespace Cosmetics
