0d6e7661bd0e223e38de2bf73c8aea1ba218694e
Seth Roffe <ssroffe@gmail.com>
2021-06-01 12:30:10 -0400

    Fixed seed cast to uint32_t in randomize all settings

--- source/settings.cpp
+++ source/settings.cpp
@@ -1115,7 +1115,8 @@ namespace Settings {
   // Hides all relevant options
   void RandomizeAllSettings() {
     //Init Random_ interface for consistency in racing
-    Random_Init(seed);
+
+    Random_Init(static_cast<uint32_t>(std::stoul(seed)));
 
     // Open Settings
     if (RandomizeOpen) {
