a7056e844ff84acadb393267f73e312cb56f89ed
Charade <hollo_nut@hotmail.com>
2021-06-28 00:21:25 -0400

    Added the item drop Item00Type enum
    To clarify the Ammo Drops section

--- code/src/item00.c
+++ code/src/item00.c
@@ -14,29 +14,60 @@
 
 #define THIS ((EnItem00*)thisx)
 
+typedef enum {
+    /* 0x00 */ ITEM00_RUPEE_GREEN,
+    /* 0x01 */ ITEM00_RUPEE_BLUE,
+    /* 0x02 */ ITEM00_RUPEE_RED,
+    /* 0x03 */ ITEM00_HEART,
+    /* 0x04 */ ITEM00_BOMBS_A,
+    /* 0x05 */ ITEM00_ARROWS_SINGLE,
+    /* 0x06 */ ITEM00_HEART_PIECE,
+    /* 0x07 */ ITEM00_HEART_CONTAINER,
+    /* 0x08 */ ITEM00_ARROWS_SMALL,
+    /* 0x09 */ ITEM00_ARROWS_MEDIUM,
+    /* 0x0A */ ITEM00_ARROWS_LARGE,
+    /* 0x0B */ ITEM00_BOMBS_B,
+    /* 0x0C */ ITEM00_NUTS,
+    /* 0x0D */ ITEM00_STICK,
+    /* 0x0E */ ITEM00_MAGIC_LARGE,
+    /* 0x0F */ ITEM00_MAGIC_SMALL,
+    /* 0x10 */ ITEM00_SEEDS,
+    /* 0x11 */ ITEM00_SMALL_KEY,
+    /* 0x12 */ ITEM00_FLEXIBLE,
+    /* 0x13 */ ITEM00_RUPEE_ORANGE,
+    /* 0x14 */ ITEM00_RUPEE_PURPLE,
+    /* 0x15 */ ITEM00_SHIELD_DEKU,
+    /* 0x16 */ ITEM00_SHIELD_HYLIAN,
+    /* 0x17 */ ITEM00_TUNIC_ZORA,
+    /* 0x18 */ ITEM00_TUNIC_GORON,
+    /* 0x19 */ ITEM00_BOMBS_SPECIAL
+} Item00Type;
+
 void EnItem00_rInit(Actor* thisx, GlobalContext* globalCtx) {
     EnItem00* item = THIS;
-    s16 item00Type = item->actor.params & 0x00FF;
+    s16 Item00Type = item->actor.params & 0x00FF;
     //If no ammo drops is chosen as an option, overrides the incoming ammo or magic drop with a blue rupee
     if(gSettingsContext.ammoDrops == AMMODROPS_NONE){		
-        if (item00Type == 0x04 ||
-        item00Type == 0x05 ||
-        item00Type == 0x08 ||
-        item00Type == 0x09 ||
-        item00Type == 0x0A ||
-        item00Type == 0x0B ||
-        item00Type == 0x0C ||
-        item00Type == 0x0E ||
-        item00Type == 0x0F ||
-        item00Type == 0x10 ||
-        item00Type == 0x12 ||
-        item00Type == 0x19) {
+        switch(Item00Type){
+            case ITEM00_BOMBS_A:
+            case ITEM00_ARROWS_SINGLE:
+            case ITEM00_ARROWS_SMALL:
+            case ITEM00_ARROWS_MEDIUM:
+            case ITEM00_ARROWS_LARGE:
+            case ITEM00_BOMBS_B:
+            case ITEM00_NUTS:
+            case ITEM00_MAGIC_LARGE:
+            case ITEM00_MAGIC_SMALL:
+            case ITEM00_SEEDS:
+            case ITEM00_FLEXIBLE:
+            case ITEM00_BOMBS_SPECIAL:
             item->actor.params = (item->actor.params & 0xFF00) | 0x01;
+			break;
         }
     }
     //If no health drops is chosen as an option, overrides the incoming health drop with a green rupee
-    if(gSettingsContext.heartDropRefill == HEARTDROPREFILL_NODROP || gSettingsContext.heartDropRefill == HEARTDROPREFILL_NODROPREFILL){	
-        if (item00Type == 0x03){
+    if((gSettingsContext.heartDropRefill == HEARTDROPREFILL_NODROP) || (gSettingsContext.heartDropRefill == HEARTDROPREFILL_NODROPREFILL)){	
+        if (Item00Type == ITEM00_HEART){
             item->actor.params = (item->actor.params & 0xFF00) | 0x00;
         }
     }
