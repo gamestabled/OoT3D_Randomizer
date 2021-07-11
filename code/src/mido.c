0845bfb8919e251f12741d2bebb8916799cff520
cjohnson57 <C.Johnson.57@outlook.com>
2021-07-10 23:50:19 -0500

    Make mido a mostly good boy
    As far as I know it shouldn't cause any issues to just despawn him in forest after showing him sword and shield  ¯\_(ツ)_/¯
    But still need to fix his dialog priority for dungeon ER

--- code/src/mido.c
+++ code/src/mido.c
@@ -4,19 +4,15 @@
 
 
 u32 EnMd_ShouldSpawn(void) {
-    bool DekuTreeDead = (gSaveContext.eventChkInf[0] & 0x80); //"Obtained Kokiri Emerald & Deku Tree Dead"
-    bool ObtainedLetter = (gSaveContext.eventChkInf[4] & 0x1); //"Obtained Zelda's Letter"
     bool ShowedSword = (gSaveContext.eventChkInf[0] & 0x10); //"Showed Mido Sword & Shield"
-    bool SpokeAfterDeath = (gSaveContext.eventChkInf[1] & 0x1000); //"Spoke to Mido After Deku Tree's Death"
-    bool DontSpawnForest = ShowedSword && (DekuTreeDead || ObtainedLetter);
-    if (gGlobalContext->sceneNum == 0x55) { //Kokiri Forest, spawn if conditions to disappear not met
-        if (!DontSpawnForest) {
+    if (gGlobalContext->sceneNum == 0x55) { //Kokiri Forest, spawn if hasn't been showed sword and shield yet
+        if (!ShowedSword) {
             return 1;
         }
     }
 
-    if (gGlobalContext->sceneNum == 0x28) { //Mido's house, spawn if conditiosn to disappear are met and child
-        if (DontSpawnForest && gSaveContext.linkAge == AGE_CHILD) {
+    if (gGlobalContext->sceneNum == 0x28) { //Mido's house, spawn if not spawning in forest
+        if (ShowedSword && gSaveContext.linkAge == AGE_CHILD) {
             return 1;
         }
     }
