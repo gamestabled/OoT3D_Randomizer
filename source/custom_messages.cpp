1dd0fd36493d9d2660d5c94817cd66263887a608
gymnast86 <gymnast86@users.noreply.github.com>
2022-11-22 01:17:37 +0000

    Run automatic format script as code does not match clang format rules.
    Developers please rebase to avoid merge conflicts!

--- source/custom_messages.cpp
+++ source/custom_messages.cpp
@@ -117,54 +117,17 @@ constexpr std::array ItalianDungeonArticles = {
 // clang-format on
 
 constexpr std::array GermanDungeonNames = {
-    "Deku-Baum",
-    "Dodongos Höhle",
-    "Jabu Jabus Bauch",
-    "Waldtempel",
-    "Feuertempel",
-    "Wassertempel",
-    "Geistertempel",
-    "Schattentempel",
-    "Grund des Brunnens",
-    "Eishöhle",
-    "Teufelsturm",
-    "Gerudo Trainingsarena",
-    "Diebesversteck",
-    "Ganons Schloss",
+    "Deku-Baum",    "Dodongos Höhle",        "Jabu Jabus Bauch", "Waldtempel",         "Feuertempel",
+    "Wassertempel", "Geistertempel",         "Schattentempel",   "Grund des Brunnens", "Eishöhle",
+    "Teufelsturm",  "Gerudo Trainingsarena", "Diebesversteck",   "Ganons Schloss",
 };
 
 constexpr std::array GermanDungeonArticles = {
-    "des ",
-    "von ",
-    "von ",
-    "des ",
-    "des ",
-    "des ",
-    "des ",
-    "des ",
-    "vom ",
-    "der ",
-    "des ",
-    "der ",
-    "des ",
-    "von ",
+    "des ", "von ", "von ", "des ", "des ", "des ", "des ", "des ", "vom ", "der ", "des ", "der ", "des ", "von ",
 };
 
 constexpr std::array GermanDungeonGenitiv = {
-    "es ",
-    " ",
-    " ",
-    "s ",
-    "s ",
-    "s ",
-    "s ",
-    "s ",
-    " ",
-    " ",
-    "s",
-    " ",
-    "s ",
-    " ",
+    "es ", " ", " ", "s ", "s ", "s ", "s ", "s ", " ", " ", "s", " ", "s ", " ",
 };
 
 constexpr std::array DungeonColors = {
@@ -861,28 +824,76 @@ Text AddColorsAndFormat(Text text, const std::vector<u8>& colors /*= {}*/) {
         arrangedMessageData = "";
     }
 
-    std::string MESSAGE_END()          { return  "\x7F\x00"s; }
-    std::string WAIT_FOR_INPUT()       { return  "\x7F\x01"s; }
-    std::string HORIZONTAL_SPACE(u8 x) { return  "\x7F\x02"s + char(x); }
-    std::string GO_TO(u16 x)           { return  "\x7F\x03"s + char(x >> 8) + char(x & 0x00FF); }
-    std::string INSTANT_TEXT_ON()      { return  "\x7F\x04"s; }
-    std::string INSTANT_TEXT_OFF()     { return  "\x7F\x05"s; }
-    std::string SHOP_MESSAGE_BOX()     { return  "\x7F\x06\x00"s; }
-    std::string EVENT_TRIGGER()        { return  "\x7F\x07"s; }
-    std::string DELAY_FRAMES(u8 x)     { return  "\x7F\x08"s + char(x); }
-    std::string CLOSE_AFTER(u8 x)      { return  "\x7F\x0A"s + char(x); }
-    std::string PLAYER_NAME()          { return  "\x7F\x0B"s; }
-    std::string PLAY_OCARINA()         { return  "\x7F\x0C"s; }
-    std::string ITEM_OBTAINED(u8 x)    { return  "\x7F\x0F"s + char(x); }
-    std::string SET_SPEED(u8 x)        { return  "\x7F\x10"s + char(x); }
-    std::string SKULLTULAS_DESTROYED() { return  "\x7F\x15"s; }
-    std::string CURRENT_TIME()         { return  "\x7F\x17"s; }
-    std::string UNSKIPPABLE()          { return  "\x7F\x19"s; }
-    std::string TWO_WAY_CHOICE()       { return  "\x7F\x1A\xFF\xFF\xFF\xFF"s; }
-    std::string NEWLINE()              { return  "\x7F\x1C"s; }
-    std::string COLOR(u8 x)            { return  "\x7F\x1D"s + char(x); }
-    std::string CENTER_TEXT()          { return  "\x7F\x1E"s; }
-    std::string IF_NOT_MQ()            { return  "\x7F\x29"s; }
-    std::string MQ_ELSE()              { return  "\x7F\x2A"s; }
-    std::string MQ_END()               { return  "\x7F\x2B"s; }
+    std::string MESSAGE_END() {
+        return "\x7F\x00"s;
+    }
+    std::string WAIT_FOR_INPUT() {
+        return "\x7F\x01"s;
+    }
+    std::string HORIZONTAL_SPACE(u8 x) {
+        return "\x7F\x02"s + char(x);
+    }
+    std::string GO_TO(u16 x) {
+        return "\x7F\x03"s + char(x >> 8) + char(x & 0x00FF);
+    }
+    std::string INSTANT_TEXT_ON() {
+        return "\x7F\x04"s;
+    }
+    std::string INSTANT_TEXT_OFF() {
+        return "\x7F\x05"s;
+    }
+    std::string SHOP_MESSAGE_BOX() {
+        return "\x7F\x06\x00"s;
+    }
+    std::string EVENT_TRIGGER() {
+        return "\x7F\x07"s;
+    }
+    std::string DELAY_FRAMES(u8 x) {
+        return "\x7F\x08"s + char(x);
+    }
+    std::string CLOSE_AFTER(u8 x) {
+        return "\x7F\x0A"s + char(x);
+    }
+    std::string PLAYER_NAME() {
+        return "\x7F\x0B"s;
+    }
+    std::string PLAY_OCARINA() {
+        return "\x7F\x0C"s;
+    }
+    std::string ITEM_OBTAINED(u8 x) {
+        return "\x7F\x0F"s + char(x);
+    }
+    std::string SET_SPEED(u8 x) {
+        return "\x7F\x10"s + char(x);
+    }
+    std::string SKULLTULAS_DESTROYED() {
+        return "\x7F\x15"s;
+    }
+    std::string CURRENT_TIME() {
+        return "\x7F\x17"s;
+    }
+    std::string UNSKIPPABLE() {
+        return "\x7F\x19"s;
+    }
+    std::string TWO_WAY_CHOICE() {
+        return "\x7F\x1A\xFF\xFF\xFF\xFF"s;
+    }
+    std::string NEWLINE() {
+        return "\x7F\x1C"s;
+    }
+    std::string COLOR(u8 x) {
+        return "\x7F\x1D"s + char(x);
+    }
+    std::string CENTER_TEXT() {
+        return "\x7F\x1E"s;
+    }
+    std::string IF_NOT_MQ() {
+        return "\x7F\x29"s;
+    }
+    std::string MQ_ELSE() {
+        return "\x7F\x2A"s;
+    }
+    std::string MQ_END() {
+        return "\x7F\x2B"s;
+    }
 }
