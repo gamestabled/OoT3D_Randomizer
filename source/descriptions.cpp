7f3b5afead55b756b09a120203a645e94c0b293d
Kewlan <colin_rehn@hotmail.com>
2024-08-01 15:58:50 +0200

    Clarify WIP features, and remove regional warning

--- source/descriptions.cpp
+++ source/descriptions.cpp
@@ -53,7 +53,10 @@ string_view logicGlitchless           = "No glitches are required, but may requi
 string_view logicGlitched             = "The glitches you enable at the set difficulty\n"  //
                                         "or below may be required.\n"                      //
                                         "\n"                                               //
-                                        "In development, but still usable.";               //
+                                        "WIP feature. Allows glitch logic for the entire\n"//
+                                        "overworld and the following vanilla dungeons:\n"  //
+                                        "Deku Tree, Dodongo's Cavern, Jabu Jabu, and\n"    //
+                                        "Forest, Water, and Fire Temple.";                 //
 string_view logicNoLogic              = "Maximize randomization, All locations are\n"      //
                                         "considered available. MAY BE IMPOSSIBLE TO BEAT.";//
 string_view logicVanilla              = "For those who want to play the game normally but\n"
@@ -1327,7 +1330,7 @@ string_view randomGsLocationsDesc     = "Moves Gold Skulltulas to different loca
                                         "Business Scrub grottos, Fairy Fountains, etc.\n"  //
                                         "Some new locations are only available with certain"
                                         "settings enabled, like tricks and glitches.\n"    //
-                                        "The logic is updated for any that are moved.";    //
+                                        "WIP feature. Most dungeon locations are the same.";
                                                                                            //
 string_view gsLocGuaranteeNewDesc     = "Excludes the original location from the Gold\n"   //
                                         "Skulltula's available locations pool.\n"          //
