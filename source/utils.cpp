eb9e20918e81c3e0eddd9511fd68b813a3dcfae2
Ura Yukimitsu <UraYukimitsu@users.noreply.github.com>
2025-02-22 11:02:59 +0100

    Add CSS stylesheets for the spoiler log (#760)
    * Add stylesheet for the spoiler logs + fix issue causing the spoiler log to be invalid XML
    
    * Fix formatting issues
    
    * Fix formatting issues 2: electric boogaloo
    
    * Please let this be the last formatting fix
    
    * Fix forgotten include

--- source/utils.cpp
+++ source/utils.cpp
@@ -1,4 +1,6 @@
 #include <algorithm>
+#include <memory>
+#include <vector>
 
 #include "utils.hpp"
 
@@ -17,4 +19,39 @@ std::string SanitizedString(std::string s) {
     }
 
     return s;
+}
+
+using FILEPtr = std::unique_ptr<FILE, decltype(&std::fclose)>;
+
+bool CopyFile(FS_Archive sdmcArchive, const char* dst, const char* src) {
+    Result res = 0;
+    Handle outFile;
+    u32 bytesWritten = 0;
+    // Delete dst if it exists
+    FSUSER_DeleteFile(sdmcArchive, fsMakePath(PATH_ASCII, dst));
+
+    // Open dst destination
+    if (!R_SUCCEEDED(res = FSUSER_OpenFile(&outFile, sdmcArchive, fsMakePath(PATH_ASCII, dst),
+                                           FS_OPEN_WRITE | FS_OPEN_CREATE, 0))) {
+        return false;
+    }
+
+    if (auto file = FILEPtr{ std::fopen(src, "r"), std::fclose }) {
+        // obtain file size
+        fseek(file.get(), 0, SEEK_END);
+        const auto lSize = static_cast<size_t>(ftell(file.get()));
+        rewind(file.get());
+
+        // copy file into the buffer
+        std::vector<char> buffer(lSize);
+        fread(buffer.data(), 1, buffer.size(), file.get());
+
+        // Write the buffer to final destination
+        if (!R_SUCCEEDED(res = FSFILE_Write(outFile, &bytesWritten, 0, buffer.data(), buffer.size(), FS_WRITE_FLUSH))) {
+            return false;
+        }
+    }
+
+    FSFILE_Close(outFile);
+    return true;
 }
