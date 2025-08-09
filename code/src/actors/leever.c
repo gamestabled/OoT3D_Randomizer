baf5ac1edddc45d4074a03666b0f2e2ccdbc8bf4
HylianFreddy <82058772+HylianFreddy@users.noreply.github.com>
2025-08-09 17:58:24 +0200

    Fix invulnerable Leevers

--- code/src/actors/leever.c
+++ code/src/actors/leever.c
@@ -2,6 +2,7 @@
 #include "settings.h"
 
 #define EnReeba_SetupSurface ((EnReebaActionFunc)GAME_ADDR(0x181394))
+#define EnReeba_Die ((EnReebaActionFunc)GAME_ADDR(0x3D4CB0))
 
 s32 Leever_ShouldSurviveOutsideSand(Actor* actor) {
     return Enemizer_IsEnemyRandomized(ENEMY_LEEVER) &&
@@ -10,7 +11,13 @@ s32 Leever_ShouldSurviveOutsideSand(Actor* actor) {
 }
 
 void Leever_AfterSink(EnReeba* leever) {
-    if (Enemizer_IsEnemyRandomized(ENEMY_LEEVER) && leever->actor.parent == NULL) {
+    if (leever->actor.colChkInfo.health == 0) {
+        // Base game bug: if the Leever is killed on the same frame it hits Link, its action function will be set to
+        // `EnReeba_Die` and then `EnReeba_Sink` during the same update cycle; when this happens it doesn't drop items
+        // and, if it's a randomized enemy, it becomes an invulnerable zombie with 0 health and no colliders. Fix this
+        // by resetting the action function to `EnReeba_Die`.
+        leever->actionFunc = EnReeba_Die;
+    } else if (Enemizer_IsEnemyRandomized(ENEMY_LEEVER) && leever->actor.parent == NULL) {
         leever->actor.world.pos = leever->actor.home.pos;
         leever->actionFunc      = EnReeba_SetupSurface;
     } else {
