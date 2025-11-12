d997caa168989fcc319e172578d4a30c7723169a
viccie211 <victorremmerswaal@hotmail.com>
2025-11-12 16:05:49 +0100

    Added comments

--- code/src/actors/obj_mure3.c
+++ code/src/actors/obj_mure3.c
@@ -1,8 +1,8 @@
 #include "z3D/z3D.h"
 #include "common.h"
 
-u8 isObjMure3Updating   = 0; // global var
-u8 extraCollectibleFlag = 0x40;
+u8 isObjMure3Updating   = 0; // global variable for rupee circle rupee replacement.
+u8 extraCollectibleFlag = 0x40; // global variable for rupee circle rupee replacement.
 #define ObjMure3_Update ((ActorFunc)GAME_ADDR(0x002318ac))
 
 void ObjMure3_rUpdate(Actor* thisx, GlobalContext* globalCtx) {
