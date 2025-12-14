42d411c17c9e1e53f55a75fcc57f1c7172953b0e
viccie211 <victorremmerswaal@hotmail.com>
2025-12-14 21:29:09 +0100

    Fixed a bug with the pot fix overruling rupee circle rupees

--- code/src/item_override.c
+++ code/src/item_override.c
@@ -136,7 +136,7 @@ static ItemOverride_Key ItemOverride_GetSearchKey(Actor* actor, u8 scene, u8 ite
         u16 collectibleFlag   = currentItem->collectibleFlag;
 
         // Don't override respawning collectibles dropped by other actors (e.g. pots)
-        if ((collectibleFlag == 0x00 || collectibleFlag >= 0x20) && !currentItem->rExt.spawnedBySceneLayer) {
+        if ((currentItem->rExt.extraCollectibleFlag == 0x00 && (collectibleFlag == 0x00 || collectibleFlag >= 0x20)) && !currentItem->rExt.spawnedBySceneLayer) {
             return (ItemOverride_Key){ .all = 0 };
         }
 
