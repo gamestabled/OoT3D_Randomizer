a3fe89436c5e7033755d8144c95b54bb6435a5f5
BuffYoda21 <buffyoda21@gmail.com>
2026-03-31 21:32:27 -0500

    cleaned up python scripts

--- tools/gen_item_table.py
+++ tools/gen_item_table.py
@@ -1,4 +1,4 @@
-import json
+from utils import load_json_with_comments
 import os
 import sys
 
@@ -13,76 +13,6 @@ void ItemTable_Init() {
 """
 
 FOOTER = "}\n"
-
-# stops commentjson from being a dependency
-def strip_comments(text):
-    result = []
-    i = 0
-    length = len(text)
-
-    in_single = False   # //
-    in_multi  = False   # /* */
-    in_string = False   # "..."
-
-    while i < length:
-        char = text[i]
-        next_char = text[i + 1] if i + 1 < length else ""
-
-        # string literals
-        if not in_single and not in_multi:
-            if char == '"' and (i == 0 or text[i - 1] != '\\'):
-                in_string = not in_string
-                result.append(char)
-                i += 1
-                continue
-
-        if in_string:
-            result.append(char)
-            i += 1
-            continue
-
-        # start of a single-line comment
-        if not in_multi and char == '/' and next_char == '/':
-            in_single = True
-            i += 2
-            continue
-
-        # start of a multi-line comment
-        if not in_single and char == '/' and next_char == '*':
-            in_multi = True
-            i += 2
-            continue
-
-        # end of a single-line comment
-        if in_single and char == '\n':
-            in_single = False
-            result.append(char)
-            i += 1
-            continue
-
-        # end of a multi-line comment
-        if in_multi and char == '*' and next_char == '/':
-            in_multi = False
-            i += 2
-            continue
-
-        # skip characters inside comments
-        if in_single or in_multi:
-            i += 1
-            continue
-
-        # normal character
-        result.append(char)
-        i += 1
-
-    return ''.join(result)
-
-def load_json_with_comments(path):
-    with open(path, encoding="utf-8") as f:
-        raw = f.read()
-
-    cleaned = strip_comments(raw)
-    return json.loads(cleaned)
 
 ################## MAIN ####################
 
