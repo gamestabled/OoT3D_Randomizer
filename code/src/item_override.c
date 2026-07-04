5d71e147a9f492e131a55bffd978fb81cddb2ef7
viccie211 <victorremmerswaal@hotmail.com>
2026-07-04 15:32:03 +0200

    grew the size of the rItemOverrides array to fix an unmapped read error

--- code/src/item_override.c
+++ code/src/item_override.c
@@ -22,7 +22,7 @@
 #define READY_ON_LAND 1
 #define READY_IN_WATER 2
 
-static ItemOverride rItemOverrides[640] = { 0 };
+static ItemOverride rItemOverrides[800] = { 0 };
 static s32 rItemOverrides_Count         = 0;
 
 static ItemOverride rPendingOverrideQueue[7] = { 0 };
