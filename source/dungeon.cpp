c469dde197f36e2a8e41628b6f3631bed9aa1d63
viccie211 <victorremmerswaal@hotmail.com>
2026-07-07 09:23:44 +0200

    Update source/dungeon.cpp
    Co-authored-by: HylianFreddy <82058772+HylianFreddy@users.noreply.github.com>

--- source/dungeon.cpp
+++ source/dungeon.cpp
@@ -605,7 +605,7 @@ DungeonInfo GerudoTrainingGrounds =
                   GERUDO_TRAINING_GROUNDS_HEAVY_BLOCK_FOURTH_CHEST,
                   GERUDO_TRAINING_GROUNDS_FREESTANDING_KEY,
                   GERUDO_TRAINING_GROUNDS_BEAMOS_ROOM_HEART_1,
-                  GERUDO_TRAINING_GROUNDS_BEAMOS_ROOM_HEART_2 },
+                  GERUDO_TRAINING_GROUNDS_BEAMOS_ROOM_HEART_2, },
                 {
                     // MQ Locations
                     GERUDO_TRAINING_GROUNDS_MQ_LOBBY_RIGHT_CHEST,
