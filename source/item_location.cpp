d5a50e06d447260c9aafa8a161995a4706aed3d0
HylianFreddy <82058772+HylianFreddy@users.noreply.github.com>
2026-07-13 23:41:20 +0200

    Add check for location names length

--- source/item_location.cpp
+++ source/item_location.cpp
@@ -1795,9 +1795,14 @@ std::vector<LocationKey> childOnlyHotLocations = {
 // clang-format on
 
 void ItemLocation::OnInit(void) {
-    // Ignore hint reachability locations
-    if (type < ItemLocationType::HintStone) {
-        sInitializedItemLocations++;
+    if (type >= ItemLocationType::HintStone) {
+        return; // Ignore hint reachability locations
+    }
+
+    sInitializedItemLocations++;
+    if (name.size() > 51) { // max length for in-game menu
+        CitraPrint("ERROR! Location name too long: " + name);
+        gInitError = true;
     }
 }
 
